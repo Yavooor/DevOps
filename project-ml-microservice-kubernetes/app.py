458c6450f57cebb0b9e787c4a999a2e2d6e44b8e
Yavor Mihalski <yavor.mihalski@tui.com>
2023-05-02 15:38:40 +0000

    latest versions

--- project-ml-microservice-kubernetes/app.py
+++ project-ml-microservice-kubernetes/app.py
@@ -5,7 +5,6 @@ import logging
 import pandas as pd
 import joblib
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import GradientBoostingRegressor
 
 app = Flask(__name__)
 LOG = create_logger(app)
